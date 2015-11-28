9cbb3af3241c240c99818b709f73b1b6fb670d7e
Anubhav Saxena <saxena20@purdue.edu>
2015-11-28 11:16:45 -0500

    Completed RPSinRuby.rb

--- RPSinRuby.rb
+++ RPSinRuby.rb
@@ -4,11 +4,63 @@ RPSinRuby.rb
 This is the program version of "Rock Paper Scissor" in Ruby. Compile and execute to run.
 =end
 
+playOn = true;
 
-computerSel = 2 + rand(3) + rand(3)
-puts "What do you want to throw?"
-userInput = gets.chomp
+while playOn == true
 
-if 
+	computerSelec = rand(3)
+
+	puts "What do you want to throw? (rock/paper/scissors)"
+	userInput = gets.chomp
+
+	if userInput == "rock"
+		if computerSelec == 1
+			puts "It's a tie. Both threw rock."
+		end
+
+		if computerSelec == 2
+			puts "Computer wins, it threw paper."
+		end
+
+		if computerSelec == 3
+			puts "You win, computer threw scissors."
+		end
+	end
+
+	if userInput == "paper"
+		if computerSelec == 1
+			puts "You win, computer threw rock."
+		end
+		if computerSelec == 2
+			puts "It's a tie. Both threw paper."
+		end
+		if computerSelec == 3
+			puts "Computer wins, it threw scissors"
+		end
+	end
+
+	if userInput == "scissors"
+		if computerSelec == 1
+			puts "Computer wins, it threw rock"
+		end
+		if computerSelec == 2
+			puts "You win, computer threw paper."
+		end
+		if computerSelec == 3
+			puts "It's a tie. Both threw scissors."
+		end
+	end
+	
+	puts "Do you want to play again?"
+	asker = gets.chomp
+
+	if asker == "yes"
+		playOn = true
+	end
+
+	if asker == "no"
+		playOn = false
+	end
+end
 
 
